8b88e2b1bea4b011302422d7a7d79964c972e174
Toriumi Hidekazu <tori.bj@gmail.com>
2026-04-20 13:19:35 +0900

    ログイン時のエラー表示方法を修正

--- config/views.py
+++ config/views.py
@@ -6,7 +6,7 @@ class CustomLoginView(LoginView):
     template_name = 'login.html'
     authentication_form = EmailAuthenticationForm
 
-    def form_invalid(self, form):
-        context = self.get_context_data(form=self.authentication_form(request=self.request))
-        context['login_error'] = True
-        return self.render_to_response(context)
+    #def form_invalid(self, form):
+        #context = self.get_context_data(form=self.authentication_form(request=self.request))
+        #context['login_error'] = True
+        #return self.render_to_response(context)
